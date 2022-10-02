5805f07a813ea3ccf4d2dfd5562d0a1eaf3a6f51
GodzillaJim <jimnanjoroge76@gmail.com>
2022-10-02 11:23:16 +0300

    feat: clock work -tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders App", () => {
+  const { container } = render(<App />);
+  const appClass = container.getElementsByClassName("App");
+  expect(appClass.length).toBe(1);
 });
